814485440c7188c64706d2d45e7467f1bd4b1f71
jmonto55 <jmonto55@gmail.com>
2024-01-07 01:41:02 -0500

    Adjust timeout for production

--- app/javascript/controllers/search_controller.js
+++ app/javascript/controllers/search_controller.js
@@ -14,7 +14,7 @@ export default class extends Controller {
 
     this.queryTimeout = setTimeout(() => {
       this.postNewQuery(event.target.value);
-    }, 2000);
+    }, 2500);
 
     this.titleTargets.forEach((title) => {
       const postTitle = title.textContent.toLowerCase();
